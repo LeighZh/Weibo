bdfe13a93b0e67367d375a7a469c959f0424fdd3
LeighZh <1803124917@qq.com>
2019-03-05 11:24:47 +0000

    完成用户注册功能

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,12 +15,21 @@ class UserController extends Controller
         return view('users.show',compact('user'));
     }
 
-    public function store($request){
-        $this->validate($request,[
+    public function store(Request $request)
+    {
+        $this->validate($request, [
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show',[$user]);
     }
 }
