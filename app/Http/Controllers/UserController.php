9b88d9b8085eeadc13b49086d9fa96f8fef1404e
LeighZh <1803124917@qq.com>
2019-03-05 10:00:31 +0000

    增加CSRF验证

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,4 +14,13 @@ class UserController extends Controller
     public function show(User $user){
         return view('users.show',compact('user'));
     }
+
+    public function store($request){
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
